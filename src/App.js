5cf7aa2bb2b5a7f47b7df3e4feaad0744b92976b
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-04-13 06:38:18 -0300

    snap try1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { createRef, useEffect } from "react";
+import createScrollSnap from "scroll-snap";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const container = createRef();
+  const { bind, unbind } = createScrollSnap(container, {
+    snapDestinationX: "0%",
+    snapDestinationY: "90%",
+    timeout: 100,
+    duration: 300,
+    threshold: 0.2,
+    snapStop: false,
+  });
+  bind();
+
+  return (
+    <div className="App">
+      <div className="SnapScrollingContainer" ref={container}>
+        <div className="page bg-primary ">
+          <span>seccion titulo</span>
+        </div>
+        <div className="page bg-success">
+          <span>seccion portfolio</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
