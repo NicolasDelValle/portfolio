a52d5162f80e2f619ad7ad17ed90300099e387ed
Nicolas Del Valle <adrianchelo7@gmail.com>
2024-04-05 22:20:14 -0300

    create react app things

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import "./HomeStyle.css";
+import nico from "../../assets/Adrian_png.png";
+import ReactFullpage from "@fullpage/react-fullpage";
+
+function Home() {
+  <ReactFullpage
+    //fullpage options
+    licenseKey={"YOUR_KEY_HERE"}
+    scrollingSpeed={1000} /* Options here */
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />;
+}
+
+export default Home;
