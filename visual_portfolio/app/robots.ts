3b881170fd5e8e79d13c19aea8298e299b120f55
Nicolas Del Valle <50749255+NicolasDelValle@users.noreply.github.com>
2025-11-10 02:37:46 -0300

    feat: Implement robots.txt functionality and add favicon and icon images

---
+++ visual_portfolio/app/robots.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || "https://nicolasdelvalle.dev";
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
--- visual_portfolio/components/SideBar/SubMenu/SubMenu.tsx
+++ visual_portfolio/components/SideBar/SubMenu/SubMenu.tsx
@@ -4,16 +4,16 @@ import { ExtensionsIcon, SearchIcon, SourceControlIcon } from "../../icons";
 function SubMenu() {
   return (
     <div className='border-b-[1px] border-b-border h-fit flex items-center px-1 py-0'>
-      <button className='border-b-[1px] border-b-white p-1'>
+      <button className='border-b-[1px] border-b-white p-1' aria-label="Explorer">
         <Files width={18} />
       </button>
-      <button className=' p-1'>
+      <button className=' p-1' aria-label="Source Control">
         <SourceControlIcon width={18} />
       </button>
-      <button className=' p-1'>
+      <button className=' p-1' aria-label="Search">
         <SearchIcon width={18} />
       </button>
-      <button className=' p-1'>
+      <button className=' p-1' aria-label="Extensions">
         <ExtensionsIcon width={18} />
       </button>
 
